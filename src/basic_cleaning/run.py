7a42a5e046ef2fc117185bf2f2576734a7a1e27b
Italo Castilho <italocosilva@gmail.com>
2022-10-28 07:54:40 -0300

    Drop incorrect rows using latitude and longitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     logger.info('Converting last_review to datetime')
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Remove incorrect latitude and longitude
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Log artifact
     logger.info('Saving output artifact')
     df.to_csv(args.output_artifact, index=False)
